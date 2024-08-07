b3302616f2892d397a654e3d0a9293ca92501717
Emiliojoa <emiliojoaquinortizmalich@gmail.com>
2024-08-06 21:48:22 -0300

    ultimos cambios

--- src/router/rutas.js
+++ src/router/rutas.js
@@ -1,10 +1,12 @@
 const {Router} = require('express')
-const {obtenerId, agregar, obtener}= require('../controllers/controladores')
+const {obtenerId, agregar, obtener, actualizar, eliminar}= require('../controllers/controladores')
 
 const router = Router()
 
 router.get('/tasks', obtener)
 router.post('/tasks', agregar)
+router.put('/tasks/:id',actualizar)
+router.delete('/tasks/:id',eliminar)
 router.get('/tasks/:id', obtenerId)
 
 module.exports = router
